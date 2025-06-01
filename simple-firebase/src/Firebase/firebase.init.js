e90cec2a6a729fcd3bdf7f147d9d911e31eed6cb
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-06-01 07:49:46 +0530

    Hide the key

--- simple-firebase/src/Firebase/firebase.init.js
+++ simple-firebase/src/Firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMLANaSJ_4RjiHFDGD4UPcKLELlmAwGsI",
-  authDomain: "simple-firebase-d1b02.firebaseapp.com",
-  projectId: "simple-firebase-d1b02",
-  storageBucket: "simple-firebase-d1b02.firebasestorage.app",
-  messagingSenderId: "498860114189",
-  appId: "1:498860114189:web:f561772aab84491a00b044"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
